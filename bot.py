603785b9d4b9d485e9e525ca875227203395c28f
DvirHatuka <dvirhatuka@gmail.com>
2020-04-02 18:46:52 +0300

    changed the message content

--- bot.py
+++ bot.py
@@ -9,15 +9,39 @@ CHANNEL_ID = 111111111111111111  # the discord channel id (inside the server)
 
 client = discord.Client()
 bot = telegram.Bot(TELEGRAM_TOKEN)
+
+
 bot.send_message(chat_id=USER_ID, text="Started the bot, you will receive notification when a user connects to "
                                        "discord")
 
 
-def notify(members):
+# use yield and iterate on the result
+def find_diff(lst1, lst2):
+    ret = None
+    for item in lst1:
+        if item not in lst2:
+            ret = item
+    if ret is not None:
+        return ret
+    for item in lst2:
+        if item not in lst1:
+            ret = item
+    return ret
+
+
+def notify(members, new_members):
     msg = ""
-    for member in members:
+    try:
+        if len(members) < len(new_members):
+            msg = find_diff(members, new_members).nick + " נכנס"
+        if len(members) > len(new_members):
+            msg = find_diff(members, new_members).nick + " עזב"
+    except AttributeError:
+        print("error")
+    msg += " | ("
+    for member in new_members:
         msg += member.nick + " "
-    msg += "are talking"
+    msg += str(max(len(members), len(new_members))) + ")"
     bot.send_message(chat_id=USER_ID, text=msg)
 
 
@@ -27,16 +51,16 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    members = 0
+    members = []
     while True:
         # get the channel data
         await client.fetch_channel(CHANNEL_ID)
         channel = client.get_channel(CHANNEL_ID)
 
         # notify if there is more than one member
-        if 1 <= len(channel.members) != members:
-            notify(channel.members)
-        members = len(channel.members)
+        if 1 <= len(channel.members) and channel.members != members:
+            notify(members, channel.members)
+        members = channel.members
 
 
 client.run(DISCORD_TOKEN)
